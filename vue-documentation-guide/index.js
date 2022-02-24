ba118d2a696259fb76b8798511434fbcdfc63c88
SungHyun-Cho <chotjd329@hotmail.com>
2022-02-24 16:28:42 +0900

    feat(document): component in vue
    컴포넌트 선언을 app mount  위에서 해야한다.

--- vue-documentation-guide/index.js
+++ vue-documentation-guide/index.js
@@ -1,9 +1,19 @@
+Vue.component("todo-item", {
+  props: ["todo"],
+  template: "<li>{{todo.text}}</li>",
+});
+
 const app = new Vue({
   el: "#app",
   data: {
     message: "Hello vue!",
     seen: true,
     todos: [{ text: "a" }, { text: "b" }, { text: "c" }],
+    groceryList: [
+      { id: 0, text: "component0" },
+      { id: 1, text: "component1" },
+      { id: 2, text: "component2" },
+    ],
   },
   methods: {
     reverseMessage: function () {
