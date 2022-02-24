a734f59d436e0814c36182d38863d7780c25f216
SungHyun-Cho <chotjd329@hotmail.com>
2022-02-24 15:53:30 +0900

    feat(document): for loops in vue

--- vue-documentation-guide/index.js
+++ vue-documentation-guide/index.js
@@ -3,8 +3,10 @@ const app = new Vue({
   data: {
     message: "Hello vue!",
     seen: true,
+    todos: [{ text: "a" }, { text: "b" }, { text: "c" }],
   },
 });
 
 app.message = "i have changed data";
 app.seen = false;
+app.todos.push({ text: "d" });
