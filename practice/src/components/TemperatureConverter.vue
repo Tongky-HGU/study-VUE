7a56db4e391d426631c64e514ec8d85fd54a5fe1
SungHyun-Cho <chotjd329@hotmail.com>
2022-02-27 16:06:37 +0900

    feat(practice): add temperature converter

---
+++ practice/src/components/TemperatureConverter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    Celcius=
+    <input type="number" :value="c" @change="setC" />
+    Fahrenheit=
+    <input type="number" :value="f" @change="setF" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      c: 0,
+      f: 32,
+    };
+  },
+  methods: {
+    setC(e, c = e.target.value) {
+      this.c = c;
+      this.f = c * (9 / 5) + 32;
+    },
+    setF(e, f = e.target.value) {
+      this.f = f;
+      this.c = (f - 32) * (5 / 9);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
