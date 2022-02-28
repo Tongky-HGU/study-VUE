f0e36237a8ef8f8a255c21e632d017d02bee9f97
SungHyun-Cho <chotjd329@hotmail.com>
2022-02-28 15:20:27 +0900

    feat(practice): add simple timer

--- practice/src/pages/GUIExamples.vue
+++ practice/src/pages/GUIExamples.vue
@@ -5,17 +5,21 @@
     <CounterButton></CounterButton>
     <h2>Temperature Converter</h2>
     <TemperatureConverter></TemperatureConverter>
+    <h2>Simple Timer</h2>
+    <SimpleTimer></SimpleTimer>
   </div>
 </template>
 
 <script>
 import CounterButton from "../components/CounterButton.vue";
 import TemperatureConverter from "../components/TemperatureConverter.vue";
+import SimpleTimer from "../components/SimpleTimer.vue";
 
 export default {
   components: {
     CounterButton,
     TemperatureConverter,
+    SimpleTimer,
   },
 };
 </script>
