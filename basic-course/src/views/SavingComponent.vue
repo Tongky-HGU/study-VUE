57e8cecff5079cc426781f030103309ee114eef9
SungHyun-Cho <chotjd329@hotmail.com>
2022-03-02 16:12:56 +0900

    feat(basic): 입력 저장 컴포넌트 예시 추가

---
+++ basic-course/src/views/SavingComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    이름
+    <input type="text" v-model="name" />
+    나이
+    <input type="text" v-model.number="age" />
+    직업
+    <input type="text" v-model="job" />
+    <button @click="saveUserInfo">저장</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      age: "",
+      job: "",
+    };
+  },
+  methods: {
+    saveUserInfo() {
+      if (this.name === "") return alert("사용자이름을 입력하세요");
+      if (this.age === "" || this.age === 0)
+        return alert("사용자이름을 입력하세요");
+
+      const params = {
+        name: this.name,
+        age: this.age,
+        job: this.job,
+      };
+
+      const res = this.saveData(params);
+
+      if (res) return alert("사용자 정보가 저장되었습니다.");
+    },
+
+    saveData(params) {
+      console.log(params);
+      return true;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
