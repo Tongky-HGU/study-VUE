10403fc8b1a61075e4afcaa05369a30e803cd340
SungHyun-Cho <sunghyun.cho@linecorp.com>
2022-03-04 15:00:42 +0900

    feat(basic): add vuex example

---
+++ basic-course/src/views/VuexExample.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>cart count {{ cartCount }}</h1>
+    <h1>cart count {{ productACount }}</h1>
+
+    <h1>count: {{ count }}</h1>
+
+    <button @click="increment">increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+    cartCount() {
+      return this.$store.getters.cartCount;
+    },
+    productACount() {
+      return this.$store.getters.cartProductACount;
+    },
+  },
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
